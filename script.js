da54b53327af3b14e923db6a52bd25608a32c78e
alexkiambo821-arch <alexkiambo821@gmail.com>
2026-07-30 20:41:00 +0300

    Joycesuprise

--- script.js
+++ script.js
@@ -39,9 +39,9 @@ function openWebsite() {
     document.getElementById("special").style.display = "block";
     document.getElementById("reasons").style.display = "block";
     document.getElementById("gallery").style.display = "block";
-    document.getElementById("journey").style.display = "block";
-    document.getElementById("journey").style.display = "block";
-    document.getElementById("music").style.display = "block";
+document.getElementById("journey").style.display = "block";
+document.getElementById("ending").style.display = "block";
+document.getElementById("music").style.display = "block";
     
     
     const song = document.getElementById("loveSong");
