bad5490ee65886d7756689841c0213b72c632831
alexkiambo821-arch <alexkiambo821@gmail.com>
2026-07-30 12:39:06 +0300

    joycesuprise

--- script.js
+++ script.js
@@ -111,3 +111,22 @@ function startSparkles() {
     }, 700);
 
 }
+const observer = new IntersectionObserver((entries)=>{
+
+entries.forEach(entry=>{
+
+if(entry.isIntersecting){
+
+entry.target.classList.add("show");
+
+}
+
+});
+
+});
+
+document.querySelectorAll(".special-card").forEach(card=>{
+
+observer.observe(card);
+
+});
